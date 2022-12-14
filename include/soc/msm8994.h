f6b2f14acaef2660f2db9fe11365c59e8f76242c
Dominik Kobinski <dominikkobinski314@gmail.com>
2022-12-14 20:09:48 +0100

    soc: qcom: Add basic support for qcom msm8994
    Signed-off-by: Dominik Kobinski <dominikkobinski314@gmail.com>

---
+++ include/soc/msm8994.h
@@ -0,0 +1,10 @@
+
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2022, Dominik Kobinski <dominikkobinski314@gmail.com>
+ */
+
+#ifndef MSM8994_H_	/* Include guard */
+#define MSM8994_H_
+
+#endif // MSM8994_H_
---
+++ soc/qcom/msm8994.c
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2022, Dominik Kobinski <dominikkobinski314@gmail.com>
+ */
+
+#include <soc/msm8994.h>
+
+void soc_init(void) {
+
+}
